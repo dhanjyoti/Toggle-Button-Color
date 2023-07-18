c4a392567dcf51f08706225138c15505c0bd6176
dhanjyoti <dhanjyotir20@gmail.com>
2023-07-18 11:47:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.getElementsByClassName("container");
+function switchMode(){
+    container[0].classList.add("dark-mode")
+}
+function switchLightMode(){
+    container[0].classList.remove("dark-mode")
+}
